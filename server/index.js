13f9846d392714d4211b6d45c48a2b95a43cf450
Vishalrajputhere <singhrajputvishal03@gmail.com>
2026-01-14 20:10:54 +0530

    Fix production config (env API URL + dynamic PORT)

--- server/index.js
+++ server/index.js
@@ -20,7 +20,8 @@ mongoose
   .then(() => console.log("✅ MongoDB connected"))
   .catch((err) => console.error(err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () =>
   console.log(`🚀 Server listening on http://localhost:${PORT}`)
 );
